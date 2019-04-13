df7a705392448cb5fc2ebc11ed14fddb3a15bd3b
Hogan Lee <39949564+h0gan1ee@users.noreply.github.com>
2019-04-13 17:03:37 +0800

    Update 熟背古文一千遍.py

--- 熟背古文一千遍.py
+++ 熟背古文一千遍.py
@@ -2,14 +2,17 @@
 import numpy.random as ran
 
 # 运行参数：
-low = 2  # 挖空时*最少*挖去的文字个数
-high = 4+1  # 挖空时*最多*挖去的文字个数 + 1
+slow = 1  # 挖空时*最少*挖去的文字个数
+shigh = 2 + 1  # 挖空时*最多*挖去的文字个数 + 1
+clow = 1  # *最少*的连续文字个数
+chigh = 2 + 1  # *最多*的连续文字个数 + 1
 placeholder = '＿'  # 代替被挖空的文字的字符
 puncs = set([',', '.', '?', ':', ';', '!', '-', '"', "'", '，', '。', '？',
              '！', '：', '；', '“', '”', '‘', '’', '(', ')', '—', '\n'])  # 标点符号和换行符号
 spaces = set(['　', ' '])  # 冗余的空格
 
-csp = 0
+skp = 0
+ctn = ran.randint(clow, chigh)
 roast = ''
 with open("input.txt", mode='r', encoding='utf-8') as rawfile:
     raw = rawfile.read()
@@ -18,12 +21,16 @@ for char in raw:
         continue
     if char in puncs:
         roast = roast + char
-    elif csp == 0:
-        csp = ran.randint(low, high)
+    elif skp == 0:
+        ctn -= 1
         roast = roast + char
-    else:
-        csp -= 1
+        if ctn == 0:
+            skp = ran.randint(slow, shigh)
+    elif ctn == 0:
+        skp -= 1
         roast = roast + placeholder
+        if skp == 0:
+            ctn = ran.randint(clow, chigh)
 print(roast)
 with open("output.txt", mode='w', encoding='utf-8') as roastfile:
     roastfile.write(roast)
